357d84d20f337ebb63556bdf2264c610398bbe4c
leave3310 <sampisam3@gmail.com>
2021-03-02 21:17:56 +0800

    stop function

--- js/all.js
+++ js/all.js
@@ -1,26 +1,6 @@
 $(document).ready(function () {
-
-    //字體放大
-    $('.font-b').click(function (e) { 
+    $('.start').click(function (e) { 
         e.preventDefault()
-        $('.content p').css('font-size', '20px')
-    })
-    
-    //字體變中
-    $('.font-m').click(function (e) { 
-        e.preventDefault()
-        $('.content p').css('font-size', '16px')
-    })
-
-    //字體變小
-    $('.font-s').click(function (e) { 
-        e.preventDefault()
-        $('.content p').css('font-size', '13px')
-    })
-
-    //關掉廣告
-    $('.ad-close').click(function (e) { 
-        e.preventDefault()
-        $('.ad').fadeOut()
+        $('.box').stop().slideToggle(1000)
     })
 });
