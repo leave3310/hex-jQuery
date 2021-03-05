dff0af805feb15fb56c02f1e9b51e39dd73375a5
leave3310 <sampisam3@gmail.com>
2021-03-05 21:53:51 +0800

    add anime

--- js/all.js
+++ js/all.js
@@ -3,4 +3,8 @@ $(document).ready(function () {
         e.preventDefault()
         $('.container').toggleClass("open")
     })
+    $('.button').click(function (e) { 
+        e.preventDefault()
+        $('.box').addClass("animate__animated animate__bounce")        
+    })
 });
