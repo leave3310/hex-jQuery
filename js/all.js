2f0d5c7b86943dd9d7a51d6221432d72ff4ec5f9
leave3310 <sampisam3@gmail.com>
2021-03-08 17:55:00 +0800

    jQuery_this

--- js/all.js
+++ js/all.js
@@ -1,10 +1,6 @@
 $(document).ready(function () {
-    $('.header').click(function (e) { 
-        e.preventDefault()
-        $('.container').toggleClass("open")
-    })
-    $('.button').click(function (e) { 
-        e.preventDefault()
-        $('.box').addClass("animate__animated animate__bounce")        
-    })
+    $('.menu li').click(function (e) { 
+        e.preventDefault();
+        $(this).toggleClass('active');
+    });
 });
