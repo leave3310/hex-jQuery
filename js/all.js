f6121d878013f0da8edec5af03f7f09554fdd3dd
leave3310 <sampisam3@gmail.com>
2021-03-10 14:56:13 +0800

    findAdvanced

--- js/all.js
+++ js/all.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
-    $('.cart li').click(function (e) { 
-
-        $(this).find('h3').toggleClass('active');
+    $('.question h3').click(function (e) { 
+        //對自己新增class
+        $(this).toggleClass('active')
+        //把回答顯示出來
+        $(this).parent().find('p').slideToggle();
+        //收起其他的回答
+        $(this).parent().siblings().find('p').slideUp();
+        //把其他選到的部分移除Class
+        $(this).parent().siblings().find('h3').removeClass('active')
     });
 });
